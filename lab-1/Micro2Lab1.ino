e39fb846d555ecc18f8a38cb6ca0fe95c5e60e6e
Thomas O'Neil <othomas475@gmail.com>
2024-02-02 16:20:01 -0500

    First Code

---
+++ lab-1/Micro2Lab1.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  char GREEN = 55; //assign pins to specific colors
+  char YELLOW = 54; 
+  char RED = 53;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
