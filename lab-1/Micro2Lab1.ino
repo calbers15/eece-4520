c449d3ccb5b38fb046a9a10de1f1fb9561734a6a
gardorider <othomas475@gmail.com>
2024-02-10 15:41:44 -0500

    Update Micro2Lab1.ino

--- lab-1/Micro2Lab1.ino
+++ lab-1/Micro2Lab1.ino
@@ -3,6 +3,15 @@ void setup() {
   char GREEN = 55; //assign pins to specific colors
   char YELLOW = 54; 
   char RED = 53;
+
+  char BUZZER = 2;
+  char Button = 52;
+  
+  pinMode(GREEN, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(Button, INPUT);
 }
 
 void loop() {
