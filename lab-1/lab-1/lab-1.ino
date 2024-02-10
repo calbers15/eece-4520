add04b202d9117aae696fefe3e37a1821be3479f
gardorider <othomas475@gmail.com>
2024-02-10 17:50:38 -0500

    Wrote the skeleton for the previously empty lab-1.ino
    need to finish TimerSample.ino to continue though

--- lab-1/lab-1/lab-1.ino
+++ lab-1/lab-1/lab-1.ino
@@ -1,20 +1,64 @@
+//#include "TimerSample.ino"
+#define GREEN 55 //assigning pins specific values without using globals
+#define YELLOW 54
+#define RED 53
+#define BUZZER 2
+#define Button 52
+#define GREEN_LEFT_ARROW 51
+
 void setup() {
   // put your setup code here, to run once:
-  char GREEN = 55; //assign pins to specific colors
-  char YELLOW = 54; 
-  char RED = 53;
 
-  char BUZZER = 2;
-  char Button = 52;
   
   pinMode(GREEN, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(RED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(Button, INPUT);
+  pinMode(GREEN_LEFT_ARROW, OUTPUT);
+
+  while(digitalRead(Button) == LOW)
+  {
+    RedFlashing();
+  }
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
 
+  //some code to make red light flash 1-1 for 3 sec
+  //some function to buzz the buzzer
+  
+  //some for loop for GLA to be solid for 3 seconds
+  
+  //some for loop for flahsing GLA for 3 seconds (on 1 sec-off 1 sec(aka 1-1))
+  //some function to buzz the buzzer
+
+  //some code to make green light stay solid for 9 seconds
+
+  //some code to make green light flash 1-1 for 3 seconds
+  //some function to buzz the buzzer
+  
+  //some code to make yellow light solid for 3 seconds
+  //some function to buzz the buzzer
+
+  //some code to make red light be for 17 sec
+
+  
+}
+
+void RedFlashing(){
+  digitalWrite(RED,HIGH);
+  //wait
+  digitalWrite(RED,LOW);
+  //wait
+  return;
+}
+
+void GLA(){
+  digitalWrite(GREEN_LEFT_ARROW, HIGH);
+  //wait
+  digitalWrite(GREEN_LEFT_ARROW, LOW);
+  //wait
 }
