b2ec41e08ea6993f9ca4b10e9b5ca170486be299
gardorider <othomas475@gmail.com>
2024-02-15 18:51:26 -0500

    deef not viris

--- lab-1/lab-1/lab-1.ino
+++ lab-1/lab-1/lab-1.ino
@@ -17,6 +17,25 @@ void setup() {
   pinMode(Button, INPUT);
   pinMode(GREEN_LEFT_ARROW, OUTPUT);
 
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();//allow interrupts
+
+  
+
   while(digitalRead(Button) == LOW)
   {
     RedFlashing();
@@ -28,7 +47,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   //some code to make red light flash 1-1 for 3 sec
+  GLAFlashing();
   //some function to buzz the buzzer
+  
   
   //some for loop for GLA to be solid for 3 seconds
   
@@ -48,6 +69,18 @@ void loop() {
   
 }
 
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
+//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+  if (toggle1){
+    digitalWrite(13,HIGH);
+    toggle1 = 0;
+  }
+  else{
+    digitalWrite(13,LOW);
+    toggle1 = 1;
+  }
+}
+
 void RedFlashing(){
   digitalWrite(RED,HIGH);
   //wait
@@ -56,7 +89,7 @@ void RedFlashing(){
   return;
 }
 
-void GLA(){
+void GLAFlashing(){
   digitalWrite(GREEN_LEFT_ARROW, HIGH);
   //wait
   digitalWrite(GREEN_LEFT_ARROW, LOW);
